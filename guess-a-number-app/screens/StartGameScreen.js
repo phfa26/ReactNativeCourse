f5ea4208710808816a7ccd23a54685e05b3ab62f
phfa26 <phfa@outlook.com.au>
2020-04-27 21:56:54 +1000

    creates start game screen

--- guess-a-number-app/screens/StartGameScreen.js
+++ guess-a-number-app/screens/StartGameScreen.js
@@ -1,21 +1,81 @@
-import React from 'react';
-import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import Card from '../components/Card'
+import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
+import Colors from '../constants/colors'
 
 const StartGameScreen = (props) => {
+
+    const [enteredValue, setEnteredValue] = useState('')
+    const [confirmed, setConfirmed] = useState(false)
+    const [selectedNumber, setSelectednumber] = useState()
+
+
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('')
+        setConfirmed(false)
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number.', 'Number has to be between 1 and 99.', [{ text: 'Ok', style: 'destructive', onPress: resetInputHandler }])
+            return
+        }
+        setConfirmed(true)
+        setSelectednumber(chosenNumber)
+        setEnteredValue('')
+        Keyboard.dismiss();
+
+    }
+
+    let confirmedOutput
+    if (confirmed) {
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title='START GAME!' onPress={() => { props.onStartGame(selectedNumber) }} />
+            </Card>)
+    }
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <TextInput />
-                <View style={styles.buttonContainer}>
-                    <Button title='Reset' onPress={() => { }} />
-                    <Button title='Confirm' onPress={() => { }} />
-                </View>
-            </Card>
-        </View>
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input
+                        style={styles.input}
+                        blurOnSubmitgg
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType='number-pad'
+                        maxLength={2}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title='Reset' onPress={resetInputHandler} color={Colors.accent} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title='Confirm' onPress={confirmInputHandler} color={Colors.primary} />
+                        </View>
+                    </View>
+                </Card>
+                {confirmedOutput}
+            </View >
+        </TouchableWithoutFeedback >
     );
 }
 
@@ -39,6 +99,17 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-around',
         padding: 15
+    },
+    button: {
+        width: 100
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: "center"
     }
 });
 
