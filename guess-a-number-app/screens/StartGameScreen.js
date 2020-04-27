245f9d46a2bd1256b1693b411a01080840d10cf2
phfa26 <phfa@outlook.com.au>
2020-04-27 15:37:54 +1000

    creates a card component and integrate it in 1screen

--- guess-a-number-app/screens/StartGameScreen.js
+++ guess-a-number-app/screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
 
+import Card from '../components/Card'
+
 const StartGameScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game!</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
                     <Button title='Reset' onPress={() => { }} />
                     <Button title='Confirm' onPress={() => { }} />
                 </View>
-            </View>
+            </Card>
         </View>
     );
 }
@@ -30,15 +32,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.6,
-        elevation: 13,
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 15
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
